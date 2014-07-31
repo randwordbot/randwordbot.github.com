cb0a86f5ef0255d463cedc45836a179e3be76a35
Venkat Naidu <Venkat.Naidu@e-hps.com>
2014-07-31 07:51:28 -0400

    first

---
+++ randomwordbot.rb
@@ -0,0 +1,20 @@
+$word = String.new
+$lengthmax = 10
+
+$letters = "abcdefghijklmnopqrstuvwxyz"
+$size = $letters.length;
+
+$f = File.new("newfile.txt",  "w+")
+
+$x = 0
+while File.stat("newfile.txt").size < 12000000 do
+	$wordlength = rand($lengthmax);
+	$newletters = 0
+	$word = String.new
+	while $wordlength>$newletters do
+	 	$word[$newletters] = $letters[rand($size)]
+	 	$newletters = $newletters+1;
+	end
+	$f.write($word.to_s + " ")
+	
+end
