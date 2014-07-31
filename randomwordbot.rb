5bd0373ee05a0c3958a29c6e6484ebb1a037fc89
Venkat Naidu <Venkat.Naidu@e-hps.com>
2014-07-31 10:04:08 -0400

    new filez

--- randomwordbot.rb
+++ randomwordbot.rb
@@ -3,18 +3,40 @@ $lengthmax = 10
 
 $letters = "abcdefghijklmnopqrstuvwxyz"
 $size = $letters.length;
+$filecount = 0;
 
-$f = File.new("newfile.html",  "w+")
+#first time init
+$time = Time.new
+$filename = $time.to_i.to_s + ".html"
 
-$x = 0
-while File.stat("newfile.html").size < 12000000 do
-	$wordlength = rand($lengthmax);
-	$newletters = 0
-	$word = String.new
-	while $wordlength>$newletters do
-	 	$word[$newletters] = $letters[rand($size)]
-	 	$newletters = $newletters+1;
+while $filecount < 2
+	$f = File.new($filename,  "w+")
+	$x = 0
+	# $f.write($word.to_s+ " ")
+	$wordcount = 0;
+	$firstWord = '';
+	$bigString = '';
+	while $wordcount < 1000 do	
+		$wordlength = rand($lengthmax);
+		$newletters = 0
+		$word = String.new
+		while $wordlength>$newletters do
+	 		$word[$newletters] = $letters[rand($size)]
+	 		$newletters = $newletters+1;
+		end
+		$bigString += $word.to_s + " ";
+		$wordcount += 1;
 	end
-	$f.write($word.to_s + " ")
-	
+	# $f.write("\n<a href=''>"+firstWord+"</a>")
+
+	#setup new filename
+	$time = Time.new
+	$filename = $time.to_i.to_s + ".html"
+	$filecount += 1;
+	$f.write($bigString + "<br><br><center><a href="+ "'./" +$filename + "'" + ">"+"NEXT" + "</a></center></br>");
 end
+
+system 'git add --all'
+system 'git commit -a -m "new filez"'
+system 'git pull origin master'
+system 'git push origin master'
